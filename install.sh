5752b303e91d44ec07fd26d9da2ebe3e555a30bf
Ricowei <124418698+Ricowei520@users.noreply.github.com>
2025-06-15 22:32:35 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,29 @@
+SKIPMOUNT=false
+PROPFILE=true
+POSTFSDATA=false
+LATESTARTSERVICE=false
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "        FCMHostsForChina       "
+  ui_print "          By MinaMiGo          "
+  ui_print "*******************************"
+}
+
+REPLACE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+REPLACE="
+"
+
+on_install() {
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+
+set_permissions() {
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+}
